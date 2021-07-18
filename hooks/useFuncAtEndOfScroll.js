cfff48967c7ebfa11b77038a5cf0646a030ef0a2
Yago Crispim <34103098+YagoCrispim@users.noreply.github.com>
2021-07-18 13:14:33 -0300

    aumento na sensibilidade do 'load scroll'

--- hooks/useFuncAtEndOfScroll.js
+++ hooks/useFuncAtEndOfScroll.js
@@ -4,9 +4,9 @@ export default function useFuncAtEndOfScroll(action) {
   useEffect(() => {
     window.onscroll = function () {
       const howMuchToTheEnd = document.documentElement.scrollHeight - window.innerHeight
-      const scrolled = window.scrollY
+      const scrolled = (window.scrollY + 100)
 
-      if (scrolled === howMuchToTheEnd) return action()
+      if ((scrolled + 100) >= howMuchToTheEnd) return action()
     }
   }, [action])
 }
